61cf4ff38d9a837919731b5d744dfc4afd92dd69
K.K <koceto1973@yahoo.ca>
2018-09-26 11:01:34 +0300

    initial commit

---
+++ Smack-Chat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack-Chat
+//
+//  Created by К.К. on 26.09.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
