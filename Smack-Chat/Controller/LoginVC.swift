4e1a3f0ca5b709393fc64d363fc443a3aae41355
K.K <koceto1973@yahoo.ca>
2018-10-07 23:35:15 +0300

    LoginVC items added, formatted and constrained, file for LoginVC added, close button connected to action

---
+++ Smack-Chat/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack-Chat
+//
+//  Created by К.К. on 7.10.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ClosePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    
+}
