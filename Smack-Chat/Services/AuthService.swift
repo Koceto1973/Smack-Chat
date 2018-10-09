e9421507a1b9f904d747317dbed7c8f3022f0fe4
K.K <koceto1973@yahoo.ca>
2018-10-09 23:56:20 +0300

    Services folder, AuthService file in

---
+++ Smack-Chat/Services/AuthService.swift
@@ -0,0 +1,65 @@
+//
+//  AuthService.swift
+//  Smack-Chat
+//
+//  Created by К.К. on 9.10.18.
+//  Copyright © 2018 TeamK. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AuthService {
+    static let instance = AuthService()
+    
+    let defaults = UserDefaults.standard
+    
+    var isLoggedIn : Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken : String {
+        get {
+            return defaults.string(forKey: TOKEN_KEY)! as String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail : String {
+        get {
+            return defaults.string(forKey: USER_EMAIL)! as String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser( email:String, password:String, completion: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type": "application/json; charset=utf-8"
+        ]
+        
+        let body = [
+            "email" : lowerCaseEmail,
+            "password" : password
+        ]
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            if response.result.error == nil {
+                completion(true)
+            } else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+}
--- Smack-Chat/Utillities/Constants.swift
+++ Smack-Chat/Utillities/Constants.swift
@@ -8,6 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool) -> ()
+
+// URL Constants
+let BASE_URL = "https://some-smack-chat.herokuapp.com" + "/v1"   // app api domain from heroku, version 1
+let URL_REGISTER = "\(BASE_URL)/account/register"
+
+// Segues
 let TO_LOGIN = "ToLogin"
 let TO_CREATE_ACCOUNT = "ToCreateAccount"
 let UNWIND = "UnwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
