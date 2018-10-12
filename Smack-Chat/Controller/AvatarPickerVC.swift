e7d74eac2f5370c9610ab1cd89222a4385bb6f5d
K.K <koceto1973@yahoo.ca>
2018-10-12 20:05:31 +0300

    AvatarPickerVC collection view working with images and avatars are picked

--- Smack-Chat/Controller/AvatarPickerVC.swift
+++ Smack-Chat/Controller/AvatarPickerVC.swift
@@ -15,6 +15,9 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     @IBOutlet weak var collectionView: UICollectionView!
     
+    // Variables
+    var avatarType = AvatarType.dark
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -34,10 +37,33 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            cell.configureCell(index: indexPath.item, type: avatarType)
             return cell
         }
         
         return AvatarCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numberOfColumns:CGFloat = 3
+        if UIScreen.main.bounds.width > 320 { // bigger than the smallest screen
+            numberOfColumns = 4
+        }
+        
+        let spaceBetweenCells:CGFloat = 10
+        let padding:CGFloat = 40
+        let cellDimention = ((collectionView.bounds.width - padding) - (numberOfColumns - 1)*spaceBetweenCells) / numberOfColumns
+        
+        return CGSize(width: cellDimention, height: cellDimention )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func backPressed(_ sender: Any) {
@@ -45,6 +71,12 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     @IBAction func segmentControlChanged(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0 {
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
+        collectionView.reloadData()
     }
     
 
