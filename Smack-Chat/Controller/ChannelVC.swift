a3e1e6458d502145ff9e5542cc4e072ca3023a89
K.K <koceto1973@yahoo.ca>
2018-10-13 14:16:27 +0300

    avatar color decoding via scanner

--- Smack-Chat/Controller/ChannelVC.swift
+++ Smack-Chat/Controller/ChannelVC.swift
@@ -11,6 +11,7 @@ import UIKit
 class ChannelVC: UIViewController {
     
     // Outlets
+    @IBOutlet weak var userImage: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     
     override func viewDidLoad() {
@@ -18,6 +19,20 @@ class ChannelVC: UIViewController {
         
         // rear view takes all but 60 points
         self.revealViewController()?.rearViewRevealWidth = self.view.frame.size.width - 60
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+    }
+    
+    @objc func userDataDidChange(_ notif:Notification) {
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        } else {
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
