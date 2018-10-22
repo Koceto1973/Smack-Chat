9960ec2295f2ca59dc0bc2f85e001f61dcd7bc28
K.K <koceto1973@yahoo.ca>
2018-10-22 10:23:39 +0300

    avatar coloring debugged

--- Smack-Chat/Services/UserDataService.swift
+++ Smack-Chat/Services/UserDataService.swift
@@ -32,7 +32,7 @@ class UserDataService {
     func returnUIColor( components:String) -> UIColor {
         let scanner = Scanner(string:components)
         let skipped = CharacterSet(charactersIn: "[], ")
-        let comma = CharacterSet(charactersIn: ".")
+        let comma = CharacterSet(charactersIn: ",")
         scanner.charactersToBeSkipped = skipped
         
         var r,g,b,a: NSString?
