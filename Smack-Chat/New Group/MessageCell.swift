28bea1128d5d9fa6178ca2ec68be28fcb6cd3723
K.K <koceto1973@yahoo.ca>
2018-10-24 23:21:23 +0300

    time stamps formats ok

--- Smack-Chat/New Group/MessageCell.swift
+++ Smack-Chat/New Group/MessageCell.swift
@@ -27,6 +27,23 @@ class MessageCell: UITableViewCell {
         userNameLabel.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        // 2018-10-24T17:50:34:456Z
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = String(isoDate[..<end])
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLabel.text = finalDate
+        }
+        
     }
 
 }
